8391925512c0d33c666768051da84904629208b2
hasibahmeds <113776613+hasibahmeds@users.noreply.github.com>
2026-03-04 14:14:29 -0600

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { loginUser,registerUser } from "../controllers/userController.js"
+import { loginUser,registerUser,getProfile,updateProfile } from "../controllers/userController.js"
+import authMiddleware from "../middleware/auth.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
+userRouter.get("/getprofile",authMiddleware,getProfile)
+userRouter.post("/updateprofile",authMiddleware,updateProfile)
 
-export default userRouter;  
+export default userRouter;
