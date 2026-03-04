a29ab77dc32a3f702b852c99ae04cd7d88cf3d40
hasibahmeds <113776613+hasibahmeds@users.noreply.github.com>
2026-03-04 00:49:09 -0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,24 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   await mongoose
+//     .connect(
+//       'mongodb+srv://chadweekboseman1000_user:chadweek@cluster0.ukvpqh1.mongodb.net/food-del'
+//     )
+//     .then(() => console.log("DB Connected"));
+// };
+
+
+
+
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      'mongodb+srv://chadweekboseman1000_user:chadweek@cluster0.ukvpqh1.mongodb.net/food-del'
-    )
+    //     .connect(
+    //   'mongodb+srv://chadweekboseman1000_user:chadweek@cluster0.ukvpqh1.mongodb.net/food-del'
+    // )
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("DB Connected"));
 };
+
