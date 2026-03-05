98b268eb352b17246c83197e590c4fea0ffec79d
hasibahmeds <113776613+hasibahmeds@users.noreply.github.com>
2026-03-05 00:11:22 -0600

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,8 +4,7 @@ const userSchema = new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     cartData:{type:Object,default:{}},
-    firstName:{type:String,required:true},
-    lastName:{type:String,required:true},
+    fullName:{type:String,required:true},
     address:{type:String,default:""},
     phone:{type:String,default:""}
 },{minimize:false})
